09477a8ddda6efd2118e437a6c61634ca1bb73d4
winger <funnywwh@qq.com>
2026-06-11 19:35:20 +0800

    test(phase10): add type is usize contract

--- tests/verify_native_cmd_build_stage1.sh
+++ tests/verify_native_cmd_build_stage1.sh
@@ -68,6 +68,7 @@ bash "$SCRIPT_DIR/verify_native_local_table_init_contract.sh"
 bash "$SCRIPT_DIR/verify_native_local_table_init_control_flow_gap_contract.sh"
 bash "$SCRIPT_DIR/verify_native_reachability_init_contract.sh"
 bash "$SCRIPT_DIR/verify_native_type_is_i32_contract.sh"
+bash "$SCRIPT_DIR/verify_native_type_is_usize_contract.sh"
 bash "$SCRIPT_DIR/verify_native_cmd_build_regression_boundary.sh"
 bash "$SCRIPT_DIR/verify_native_cmd_build_no_silent_c99.sh"
 
